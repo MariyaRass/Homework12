984db4a2277abe5bb9a355eda3e330791123eaca
Мария <git@github.com:MariyaRass/helloworld.git>
2024-03-18 21:18:30 +0300

    12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author tolstoy = new Author("Лев", "Толстой");
+        Author gogol = new Author("Николай", "Гоголь");
+
+        Book warAndPeace = new Book("Война и мир", 1867, tolstoy);
+        Book theAuditor = new Book("Ревизор", 1835, gogol);
+
+        print(warAndPeace);
+        print(theAuditor);
+
+        theAuditor.setYear(1836);
+        print(theAuditor);
+    }
+
+    private static void print(Book book) {
+        System.out.println("Автор: " + book.getAuthor().getName() + " " + book.getAuthor().getSurname() +
+                ", название: " + book.getTitle() + ", год издания: " + book.getYear());
     }
 }
+
+
+
